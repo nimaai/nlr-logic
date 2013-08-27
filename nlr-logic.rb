ed89abb410350a855429b5b583a0ee9bc1a5dcd6
Matus Kmit <matus.kmit@zhdk.ch>
2013-08-27 12:15:42 +0200

    simplifying and working with Time class

--- nlr-logic.rb
+++ nlr-logic.rb
@@ -1,16 +1,32 @@
+require "date"
+require "pry"
+
 module NLRLogic
 
-  TIMES = [{day_part: "nishanta", start: {hour: 3, minute: 36}, end: {hour: 5, minute: 59}},
-           {day_part: "prataha", start: {hour: 6, minute: 00}, end: {hour: 8, minute: 23}},
-           {day_part: "purvahna", start: {hour: 8, minute: 24}, end: {hour: 10, minute: 47}},
-           {day_part: "madhyahna", start: {hour: 10, minute: 48}, end: {hour: 15, minute: 35}},
-           {day_part: "aparahna", start: {hour: 15, minute: 36}, end: {hour: 17, minute: 59}},
-           {day_part: "shayana", start: {hour: 18, minute: 00}, end: {hour: 20, minute: 23}},
-           {day_part: "pradosha", start: {hour: 20, minute: 24}, end: {hour: 22, minute: 47}},
-           {day_part: "nisha", start: {hour: 22, minute: 48}, end: {hour: 3, minute: 35}}]
+  DAY_PARTS = [{name: "nishanta", start_hour: 3, start_minute: 36},
+               {name: "prataha", start_hour: 6, start_minute: 00},
+               {name: "purvahna", start_hour: 8, start_minute: 24},
+               {name: "madhyahna", start_hour: 10, start_minute: 48},
+               {name: "aparahna", start_hour: 15, start_minute: 36},
+               {name: "shayana", start_hour: 18, start_minute: 00},
+               {name: "pradosha", start_hour: 20, start_minute: 24},
+               {name: "nisha", start_hour: 22, start_minute: 48}]
 
-  def self.day_minute hour, minute
-    (hour * 60) + minute
+  class NLRTime < ::Time
+    def initialize(hour, minute)
+      now = Time.now
+      super(now.year, now.month, now.day, hour, minute)
+    end
+  end
+
+  def self.which_day_part? hour, minute
+    t = NLRTime.new hour, minute
+    DAY_PARTS.each_with_index do |day_part, i|
+      if t.between? NLRTime.new(day_part[:start_hour], day_part[:start_minute]),
+                    NLRTime.new(DAY_PARTS[i + 1][:start_hour], DAY_PARTS[i + 1][:start_minute])
+        return day_part[:name]
+      else next end
+    end
   end
 
 end
