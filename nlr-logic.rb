45286d96f6a1d75a7e076d68a7da18a8bd2f06a2
Matus Kmit <matus.kmit@zhdk.ch>
2013-09-21 00:27:23 +0200

    integrate next day part index into DAY_PARTS

--- nlr-logic.rb
+++ nlr-logic.rb
@@ -5,9 +5,11 @@ Bundler.require
 module NLRLogic
 
   class DayPart
-    attr_reader :name, :start
-    def initialize(name: "", start: NLRTime.new(0, 0))
-      @name, @start = name, start
+    attr_reader :name, :start, :next_index
+    def initialize(options = {})
+      @name = options[:name]
+      @start = options[:start]
+      @next_index = options[:next_index]
     end
   end
 
@@ -18,18 +20,18 @@ module NLRLogic
     end
   end
 
-  DAY_PARTS = CircularList.new [DayPart.new(name: "nishanta", start: NLRTime.new(3, 36)),
-                                DayPart.new(name: "prataha", start: NLRTime.new(6, 0)),
-                                DayPart.new(name: "purvahna", start: NLRTime.new(8, 24)),
-                                DayPart.new(name: "madhyahna", start: NLRTime.new(10, 48)),
-                                DayPart.new(name: "aparahna", start: NLRTime.new(15, 36)),
-                                DayPart.new(name: "shayana", start: NLRTime.new(18, 0)),
-                                DayPart.new(name: "pradosha", start: NLRTime.new(20, 24)),
-                                DayPart.new(name: "nisha", start: NLRTime.new(22, 48))]
+  DAY_PARTS = [DayPart.new(name: "nishanta", start: NLRTime.new(3, 36), next_index: 1),
+               DayPart.new(name: "prataha", start: NLRTime.new(6, 0), next_index: 2),
+               DayPart.new(name: "purvahna", start: NLRTime.new(8, 24), next_index: 3),
+               DayPart.new(name: "madhyahna", start: NLRTime.new(10, 48), next_index: 4),
+               DayPart.new(name: "aparahna", start: NLRTime.new(15, 36), next_index: 5),
+               DayPart.new(name: "shayana", start: NLRTime.new(18, 0), next_index: 6),
+               DayPart.new(name: "pradosha", start: NLRTime.new(20, 24), next_index: 7),
+               DayPart.new(name: "nisha", start: NLRTime.new(22, 48), next_index: 0)]
 
   class DayPart
     def following
-      DAY_PARTS.fetch(Array.instance_method(:index).bind(DAY_PARTS).call(self) + 1)
+      DAY_PARTS[next_index]
     end
   end
 
